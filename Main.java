055273d549a1c62ec5ec47a035305b353a624c85
Sravani Vedam <sravanivedam@zipcoders-MacBook-Pro.local>
2019-02-06 08:25:08 -0500

    Completed SumOfInput assignment-Sravani

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
+    
+    
     public static void main(String[] args){
-
+       
+        int number;
+        int sum=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        number = scanner.nextInt();
+       
+        for(int i = 1; i<=number;i++)
+        {
+           
+            sum = sum + i;
+        
+        }
+        
+        System.out.println("The sum of number is : "+ sum);
+        
     }
 }
